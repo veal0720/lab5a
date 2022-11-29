50a8f9796103f8337ad839570db9c593aafc2429
Ruthann <r.veal0720@gmail.com>
2022-11-28 18:56:57 -0600

    sum functions work

--- js/app.js
+++ js/app.js
@@ -10,6 +10,9 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
   console.log(a,b);
+  let sumNumber = a + b;
+//   let message = 'The sum of ' + a + ' and ' + b + ' is ' + sumNumber + '.';
+  return [sumNumber, message];
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -31,7 +34,7 @@ function multiply(a, b) { //eslint-disable-line
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
